6cdd7dc7bae703d0498331ad10536b570afd5278
Soheib OUADFEL <sol@3cevolution.com>
2020-06-03 16:41:57 +0200

    Design after search

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -34,40 +34,43 @@
 			</div>
 
 
-			<div class="content">
-				<div class="container-fluid">
+			<div class="content ">
+				<div class="container-fluid" >
 
 					<div class="row">
 						<div class="col-md-12 col-lg-4 col-xl-3 theiaStickySidebar">
-						
+
 							<!-- Search Filter -->
-							<div class="card search-filter">
+							<div class="card search-filter" style=" border: 2px solid #3f729b;">
 								<div class="card-header">
-									<h4 class="card-title mb-0">Search Filter</h4>
+									<h4 class="card-title mb-0">Filtrer le résultat</h4>
 								</div>
 								<div class="card-body">
+                                    <div class="filter-widget" >
+                                        <h4>Code postal</h4>
+                                        <div >
+                                            <input type="text" class="form-control"
+                                                   style=" border: 2px solid #3f729b;"
+                                                     placeholder="Exemple : 16058 ">
+                                        </div>
+                                    </div>
 								<div class="filter-widget">
-									<div class="cal-icon">
-										<input type="text" class="form-control datetimepicker" placeholder="Select Date">
-									</div>			
-								</div>
-								<div class="filter-widget">
-									<h4>Gender</h4>
+									<h4>Sexe</h4>
 									<div>
 										<label class="custom_check">
 											<input type="checkbox" name="gender_type" checked>
-											<span class="checkmark"></span> Male Doctor
+											<span class="checkmark"></span> Médecin homme
 										</label>
 									</div>
 									<div>
 										<label class="custom_check">
 											<input type="checkbox" name="gender_type">
-											<span class="checkmark"></span> Female Doctor
+											<span class="checkmark"></span> Médecin femme
 										</label>
 									</div>
 								</div>
 								<div class="filter-widget">
-									<h4>Select Specialist</h4>
+									<h4>Sélectionner une spécialitée</h4>
 									<div>
 										<label class="custom_check">
 											<input type="checkbox" name="select_specialist" checked>
@@ -106,19 +109,19 @@
 									</div>
 								</div>
 									<div class="btn-search">
-										<button type="button" class="btn btn-block">Search</button>
-									</div>	
+										<button type="button" class="btn btn-rounded btn-secondary">Chercher</button>
+									</div>
 								</div>
 							</div>
 							<!-- /Search Filter -->
-							
+
 						</div>
-						
+
 						<div class="col-md-12 col-lg-8 col-xl-9">
 
 							<!-- Doctor Widget -->
 							@foreach(['2','2'] as $s)
-							<div class="card">
+							<div class="card" style=" border: 2px solid #3f729b;">
 								<div class="card-body">
 									<div class="doctor-widget">
 										<div class="doc-info-left">
@@ -190,14 +193,14 @@
 							@endforeach
 							<!-- /Doctor Widget -->
 							<div class="load-more text-center">
-								<a class="btn btn-primary btn-sm" href="javascript:void(0);">Load More</a>	
-							</div>	
+								<a class="btn btn-primary btn-sm" href="javascript:void(0);">Load More</a>
+							</div>
 						</div>
 					</div>
 
 				</div>
 
-			</div>		
+			</div>
 
 
 
