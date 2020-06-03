d74af0e89517d4cbf0610efafb9dcd3419c5c25a
RidaMohamed <moharedha19@gmail.com>
2020-06-03 18:34:19 +0200

    design correction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,67 +2,62 @@
 
 @section('content')
 <div class="row">
-						<div class="col-md-8 offset-md-2">
+	<div class="col-md-8 offset-md-2">
 
-						@if ($errors->any())
-							@foreach ($errors->all() as $error)
-								<div>{{$error}}</div>
-							@endforeach
-						@endif
+	@if ($errors->any())
+		@foreach ($errors->all() as $error)
+			<div>{{$error}}</div>
+		@endforeach
+	@endif
 							
-							<!-- Login Tab Content -->
-							<div class="account-content">
-								<div class="row align-items-center justify-content-center">
-									<div class="col-md-7 col-lg-6 login-left">
-										<img src="{{asset('img/login-banner.png')}}" class="img-fluid" alt="Doccure Login">	
-									</div>
-									<div class="col-md-12 col-lg-6 login-right">
-										<div class="login-header">
-											<h3>Login <span>Doccure</span></h3>
-										</div>
-                                        @isset($url)
-                                        <form method="POST" action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
-                                        @else
-                                        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                                        @endisset
-                                            @csrf
-											<div class="form-group form-focus">
-												<input type="email" name="email" class="form-control floating">
-												<label class="focus-label">Email</label>
-											</div>
-											<div class="form-group form-focus">
-												<input type="password" name="password" class="form-control floating">
-												<label class="focus-label">Password</label>
-											</div>
-											<div class="text-right">
-												<a class="forgot-link" href="forgot-password.html">Forgot Password ?</a>
-											</div>
-											<button class="btn btn-primary btn-block btn-lg login-btn" type="submit">Login</button>
-											<div class="login-or">
-												<span class="or-line"></span>
-												<span class="span-or">or</span>
-											</div>
-											<div class="row form-row social-login">
-												<div class="col-6">
-													<a href="#" class="btn btn-facebook btn-block"><i class="fab fa-facebook-f mr-1"></i> Login</a>
-												</div>
-												<div class="col-6">
-													<a href="#" class="btn btn-google btn-block"><i class="fab fa-google mr-1"></i> Login</a>
-												</div>
-											</div>
-											@isset($url)
-											<div class="text-center dont-have">Don’t have an account? <a href="{{route('register')}}">Register</a></div>
-											@else
-											<div class="text-center dont-have">Don’t have an account? <a href="{{route('register')}}">Register</a></div>
-											@endisset
-											
-										</form>
-									</div>
-								</div>
-							</div>
-							<!-- /Login Tab Content -->
-								
-						</div>
+	<!-- Login Tab Content -->
+	<div class="account-content">
+		<div class="row align-items-center justify-content-center">
+			<div class="col-md-7 col-lg-6 login-left">
+					<img src="{{asset('img/login-banner.png')}}" class="img-fluid" alt="Doccure Login">	
+			</div>
+			<div class="col-md-12 col-lg-6 login-right">
+					<div class="login-header">
+						<h3>Login <span>patient</span></h3>
 					</div>
+
+                    @isset($url)
+                 <form method="POST" action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
+                    @else
+                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @endisset
+                    @csrf
+
+					<div class="form-group form-focus">
+						<input type="email" name="email" class="form-control floating" style=" border: 1px solid #15558d;">
+						<label class="focus-label">Email</label>
+					</div>
+					<div class="form-group form-focus">
+						<input type="password" name="password" class="form-control floating" style=" border: 1px solid #15558d;">
+						<label class="focus-label">Mot de passe</label>
+					</div>
+					<div class="text-right">
+						<a class="forgot-link" href="forgot-password.html">Mot de passe oublié ?</a>
+					</div>
+
+					<button class="btn btn-primary btn-block btn-lg login-btn" type="submit">Login</button>
+						
+					<div class="login-or">
+						<span class="or-line"></span>
+						<span class="span-or">OU</span>
+					</div>
+					
+					@isset($url)
+					<div class="text-center dont-have">Nouveau sur Tebibe ? <a href="{{route('register')}}">S'inscrire</a></div>
+					@else
+				    <div class="text-center dont-have">Nouveau sur Tebibe ? <a href="{{route('register')}}">S'inscrire</a></div>
+				    @endisset
+											
+				</form>
+			</div>
+		</div>
+	</div>
+   </div>
+</div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -135,18 +135,7 @@
 
     <!-- Page Content -->
 @yield('content')
-<!-- /Page Content -->
 
-    <!-- Footer --><footer class="page-footer font-small blue">
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        </div>
-        <!-- Copyright -->
-
-    </footer>
-
-    <!-- /Footer -->
 
 </div>
 <!-- /Main Wrapper -->
