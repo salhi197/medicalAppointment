632d78297e372036f11edc4e69be73cb69eb5b17
sol <souadfel@3c-e.com>
2020-06-19 10:29:00 +0200

    conf v3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <!-- Mirrored from dreamguys.co.in/demo/doccure/template/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Mar 2020 10:57:40 GMT -->
 <head>
     <meta charset="utf-8">
-    <title>Doccure</title>
+    <title>Tebibe</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 
     <!-- Favicons -->
@@ -266,6 +266,144 @@
 <script src="{{asset('js/script.js')}}"></script>
 @yield('scripts')
 </body>
+
+<!-- Footer -->
+<footer class="footer">
+
+    <!-- Footer Top -->
+    <div class="footer-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-about">
+                        <div class="footer-logo">
+                            <img src="{{asset('img/footer-logo.png')}}" alt="logo">
+                        </div>
+                        <div class="footer-about-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <div class="social-icon">
+                                <ul>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-facebook-f"></i> </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-twitter"></i> </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" target="_blank"><i class="fab fa-dribbble"></i> </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-menu">
+                        <h2 class="footer-title">Pour les patients</h2>
+                        <ul>
+                            <li><a href="search.html">Recherche de médecins</a></li>
+                            <li><a href="login.html">Connexion</a></li>
+                            <li><a href="register.html">S'inscrire</a></li>
+                            <li><a href="booking.html">Réservation</a></li>
+                            <li><a href="patient-dashboard.html">Tableau de bord des patients</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-menu">
+                        <h2 class="footer-title">Pour les médecins</h2>
+                        <ul>
+                            <li><a href="appointments.html">Rendez-vous</a></li>
+                            <li><a href="login.html">Connexion</a></li>
+                            <li><a href="doctor-register.html">S'inscrire</a></li>
+                            <li><a href="doctor-dashboard.html">Tableau de bord des patients</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-contact">
+                        <h2 class="footer-title">Contact Us</h2>
+                        <div class="footer-contact-info">
+                            <div class="footer-address">
+                                <span><i class="fas fa-map-marker-alt"></i></span>
+                                <p> 3556  Beech Street, San Francisco,<br> California, CA 94108 </p>
+                            </div>
+                            <p>
+                                <i class="fas fa-phone-alt"></i>
+                                +1 315 369 5943
+                            </p>
+                            <p class="mb-0">
+                                <i class="fas fa-envelope"></i>
+                                tebibe@hotmail.com
+                            </p>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- /Footer Top -->
+
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        <div class="container-fluid">
+
+            <!-- Copyright -->
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <div class="copyright-text">
+                            <p class="mb-0">&copy; 2020 Tebibe. All rights reserved.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+
+                        <!-- Copyright Menu -->
+                        <div class="copyright-menu">
+                            <ul class="policy-menu">
+                                <li><a href="term-condition.html">Terms and Conditions</a></li>
+                                <li><a href="privacy-policy.html">Policy</a></li>
+                            </ul>
+                        </div>
+                        <!-- /Copyright Menu -->
+
+                    </div>
+                </div>
+            </div>
+            <!-- /Copyright -->
+
+        </div>
+    </div>
+    <!-- /Footer Bottom -->
+
+</footer>
+<!-- /Footer -->
 
 <!-- Mirrored from dreamguys.co.in/demo/doccure/template/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Mar 2020 10:57:41 GMT -->
 </html>
